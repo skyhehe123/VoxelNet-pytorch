0a5600f75468ae3ff615794ff35d3825c3c8c5bc
skyhehe123 <16903348R@connect.polyu.hk>
2018-04-09 12:12:25 +0800

    first commitment

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# VoxelNet
+# Introduction
+
+This is an unofficial implementation of [VoxelNet: End-to-End Learning for Point Cloud Based 3D Object Detection](https://arxiv.org/abs/1711.06396) in pytorch. A large part of this project is based on the work [here](https://github.com/jeasinema/VoxelNet-tensorflow)
+# Dependencies
+- `python3.5+`
+- `pytorch` (tested on 0.3.1)
+- `opencv`
+- `shapely`
+- `mayavi`
+
+# Installation
+1. Clone this repository.
+2. Compile the Cython module for box_overlaps
+```bash
+$ python3 setup.py build_ext --inplace
+```
+3. Compile the nms model
+```bash
+$ python3 nms/build.py
+```
+
+
+# Data Preparation
+1. Download the 3D KITTI detection dataset from [here](http://www.cvlibs.net/datasets/kitti/eval_object.php?obj_benchmark=3d). Data to download include:
+    * Velodyne point clouds (29 GB): input data to VoxelNet
+    * Training labels of object data set (5 MB): input label to VoxelNet
+    * Camera calibration matrices of object data set (16 MB): for visualization of predictions
+    * Left color images of object data set (12 GB): for visualization of predictions
+
+2. In this project, the cropped point cloud data for training and validation. Point clouds outside the image coordinates are removed.
+
+3. Split the training set into training and validation set according to the protocol [here](https://xiaozhichen.github.io/files/mv3d/imagesets.tar.gz). And rearrange the folders to have the following structure:
+```plain
+└── DATA_DIR
+       ├── training   <-- training data
+       |   ├── image_2
+       |   ├── label_2
+       |   └── velodyne
+       └── testing  <--- testing data
+       |   ├── image_2
+       |   ├── label_2
+       |   └── velodyne
+```
+
+# Train
+
+
+
+
+# TODO
+- [ ] improve the performances
+- [ ] reproduce results for `Pedestrian` and `Cyclist`
+- [ ] fix the deadlock problem in multi-thread processing in training
+- [ ] fix the infinite loop problem in `test.py`
+- [ ] replace averaged calibration matrices with correct ones
+
+
---
+++ data/kitti.py
@@ -0,0 +1,178 @@
+from __future__ import division
+import os
+import os.path
+import torch.utils.data as data
+import utils
+from utils import box3d_corner_to_center_batch, anchors_center_to_corner, corner_to_standup_box2d_batch, aug_data
+from box_overlaps import bbox_overlaps
+import numpy as np
+import cv2
+
+class KittiDataset(data.Dataset):
+
+    def __init__(self, cfg, root='./KITTI',set='train',type='velodyne_train'):
+        self.type = type
+        self.root = root
+        self.data_path = os.path.join(root, 'training')
+        self.lidar_path = os.path.join(self.data_path, "crop/")
+        self.image_path = os.path.join(self.data_path, "image_2/")
+        self.calib_path = os.path.join(self.data_path, "calib/")
+        self.label_path = os.path.join(self.data_path, "label_2/")
+
+        with open(os.path.join(self.data_path, '%s.txt' % set)) as f:
+            self.file_list = f.read().splitlines()
+
+        self.T = cfg.T
+        self.vd = cfg.vd
+        self.vh = cfg.vh
+        self.vw = cfg.vw
+        self.xrange = cfg.xrange
+        self.yrange = cfg.yrange
+        self.zrange = cfg.zrange
+        self.anchors = cfg.anchors.reshape(-1,7)
+        self.feature_map_shape = (int(cfg.H / 2), int(cfg.W / 2))
+        self.anchors_per_position = cfg.anchors_per_position
+        self.pos_threshold = cfg.pos_threshold
+        self.neg_threshold = cfg.neg_threshold
+
+    def cal_target(self, gt_box3d):
+        # Input:
+        #   labels: (N,)
+        #   feature_map_shape: (w, l)
+        #   anchors: (w, l, 2, 7)
+        # Output:
+        #   pos_equal_one (w, l, 2)
+        #   neg_equal_one (w, l, 2)
+        #   targets (w, l, 14)
+        # attention: cal IoU on birdview
+
+        anchors_d = np.sqrt(self.anchors[:, 4] ** 2 + self.anchors[:, 5] ** 2)
+
+        pos_equal_one = np.zeros((*self.feature_map_shape, 2))
+        neg_equal_one = np.zeros((*self.feature_map_shape, 2))
+        targets = np.zeros((*self.feature_map_shape, 14))
+
+        gt_xyzhwlr = box3d_corner_to_center_batch(gt_box3d)
+
+        anchors_corner = anchors_center_to_corner(self.anchors)
+
+        anchors_standup_2d = corner_to_standup_box2d_batch(anchors_corner)
+        # BOTTLENECK
+        gt_standup_2d = corner_to_standup_box2d_batch(gt_box3d)
+
+        iou = bbox_overlaps(
+            np.ascontiguousarray(anchors_standup_2d).astype(np.float32),
+            np.ascontiguousarray(gt_standup_2d).astype(np.float32),
+        )
+
+        id_highest = np.argmax(iou.T, axis=1)  # the maximum anchor's ID
+        id_highest_gt = np.arange(iou.T.shape[0])
+        mask = iou.T[id_highest_gt, id_highest] > 0
+        id_highest, id_highest_gt = id_highest[mask], id_highest_gt[mask]
+        # find anchor iou > cfg.XXX_POS_IOU
+        id_pos, id_pos_gt = np.where(iou > self.pos_threshold)
+        # find anchor iou < cfg.XXX_NEG_IOU
+        id_neg = np.where(np.sum(iou < self.neg_threshold,
+                                 axis=1) == iou.shape[1])[0]
+
+        id_pos = np.concatenate([id_pos, id_highest])
+        id_pos_gt = np.concatenate([id_pos_gt, id_highest_gt])
+        # TODO: uniquify the array in a more scientific way
+        id_pos, index = np.unique(id_pos, return_index=True)
+        id_pos_gt = id_pos_gt[index]
+        id_neg.sort()
+        # cal the target and set the equal one
+        index_x, index_y, index_z = np.unravel_index(
+            id_pos, (*self.feature_map_shape, self.anchors_per_position))
+        pos_equal_one[index_x, index_y, index_z] = 1
+        # ATTENTION: index_z should be np.array
+
+        targets[index_x, index_y, np.array(index_z) * 7] = \
+            (gt_xyzhwlr[id_pos_gt, 0] - self.anchors[id_pos, 0]) / anchors_d[id_pos]
+        targets[index_x, index_y, np.array(index_z) * 7 + 1] = \
+            (gt_xyzhwlr[id_pos_gt, 1] - self.anchors[id_pos, 1]) / anchors_d[id_pos]
+        targets[index_x, index_y, np.array(index_z) * 7 + 2] = \
+            (gt_xyzhwlr[id_pos_gt, 2] - self.anchors[id_pos, 2]) / self.anchors[id_pos, 3]
+        targets[index_x, index_y, np.array(index_z) * 7 + 3] = np.log(
+            gt_xyzhwlr[id_pos_gt, 3] / self.anchors[id_pos, 3])
+        targets[index_x, index_y, np.array(index_z) * 7 + 4] = np.log(
+            gt_xyzhwlr[id_pos_gt, 4] / self.anchors[id_pos, 4])
+        targets[index_x, index_y, np.array(index_z) * 7 + 5] = np.log(
+            gt_xyzhwlr[id_pos_gt, 5] / self.anchors[id_pos, 5])
+        targets[index_x, index_y, np.array(index_z) * 7 + 6] = (
+                gt_xyzhwlr[id_pos_gt, 6] - self.anchors[id_pos, 6])
+        index_x, index_y, index_z = np.unravel_index(
+            id_neg, (*self.feature_map_shape, self.anchors_per_position))
+        neg_equal_one[index_x, index_y, index_z] = 1
+        # to avoid a box be pos/neg in the same time
+        index_x, index_y, index_z = np.unravel_index(
+            id_highest, (*self.feature_map_shape, self.anchors_per_position))
+        neg_equal_one[index_x, index_y, index_z] = 0
+
+        return pos_equal_one, neg_equal_one, targets
+
+    def preprocess(self, lidar):
+
+        # shuffling the points
+        np.random.shuffle(lidar)
+
+        voxel_coords = ((lidar[:, :3] - np.array([self.xrange[0], self.yrange[0], self.zrange[0]])) / (
+                        self.vw, self.vh, self.vd)).astype(np.int32)
+
+        # convert to  (D, H, W)
+        voxel_coords = voxel_coords[:,[2,1,0]]
+        voxel_coords, inv_ind, voxel_counts = np.unique(voxel_coords, axis=0, \
+                                                  return_inverse=True, return_counts=True)
+
+        voxel_features = []
+
+        for i in range(len(voxel_coords)):
+            voxel = np.zeros((self.T, 7), dtype=np.float32)
+            pts = lidar[inv_ind == i]
+            if voxel_counts[i] > self.T:
+                pts = pts[:self.T, :]
+                voxel_counts[i] = self.T
+            # augment the points
+            voxel[:pts.shape[0], :] = np.concatenate((pts, pts[:, :3] - np.mean(pts[:, :3], 0)), axis=1)
+            voxel_features.append(voxel)
+        return np.array(voxel_features), voxel_coords
+
+    def __getitem__(self, i):
+
+        lidar_file = self.lidar_path + '/' + self.file_list[i] + '.bin'
+        calib_file = self.calib_path + '/' + self.file_list[i] + '.txt'
+        label_file = self.label_path + '/' + self.file_list[i] + '.txt'
+        image_file = self.image_path + '/' + self.file_list[i] + '.png'
+        # lidar_file = self.lidar_path + '/' + '000008' + '.bin'
+        # calib_file = self.calib_path + '/' + '000008' + '.txt'
+        # label_file = self.label_path + '/' + '000008' + '.txt'
+        # image_file = self.image_path + '/' + '000008' + '.png'
+
+        calib = utils.load_kitti_calib(calib_file)
+        Tr = calib['Tr_velo2cam']
+        gt_box3d = utils.load_kitti_label(label_file, Tr)
+        lidar = np.fromfile(lidar_file, dtype=np.float32).reshape(-1, 4)
+        lidar = utils.get_filtered_lidar(lidar)
+
+        lidar, gt_box3d = aug_data(lidar, gt_box3d)
+
+        if self.type == 'velodyne_train':
+
+            voxel_features, voxel_coords = self.preprocess(lidar)
+            pos_equal_one, neg_equal_one, targets = self.cal_target(gt_box3d)
+            return voxel_features, voxel_coords, pos_equal_one, neg_equal_one, targets
+
+        elif self.type == 'velodyne_test':
+            image = cv2.imread(image_file)
+            pos_equal_one, neg_equal_one, targets = self.cal_target(gt_box3d)
+            return lidar,image,calib,targets,pos_equal_one, self.file_list[i]
+
+        else:
+            raise ValueError('the type invalid')
+
+
+    def __len__(self):
+        return len(self.file_list)
+
+
+
